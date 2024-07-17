a9eee79223fa579d26bd0afa8144ec7c0513d513
tusharbharambe2005 <tusharbharambe2005@gmail.com>
2024-07-17 10:36:18 +0530

    simple work

--- tweet/views.py
+++ tweet/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect,
 from .models import Tweet
 from .forms import TweetForm,UserRegistrationForm
 from django.shortcuts import get_object_or_404
